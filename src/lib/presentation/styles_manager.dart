5da0404e4508d122a1ef4cdb3971bd2796b43909
Johnathon Sullinger <johnathon@sullinger.us>
2022-05-25 21:54:05 -0700

    Theme manager wip

--- src/lib/presentation/styles_manager.dart
+++ src/lib/presentation/styles_manager.dart
@@ -10,5 +10,11 @@ TextStyle getRegularStyle({double fontSize = FontSize.normal, required Color col
 TextStyle getLightStyle({double fontSize = FontSize.normal, required Color color}) =>
     _getTextStyle(fontSize, FontConstants.fontFamily, FontWeightManager.light, color);
 
+TextStyle getMediumStyle({double fontSize = FontSize.normal, required Color color}) =>
+    _getTextStyle(fontSize, FontConstants.fontFamily, FontWeightManager.medium, color);
+
 TextStyle getBoldStyle({double fontSize = FontSize.normal, required Color color}) =>
     _getTextStyle(fontSize, FontConstants.fontFamily, FontWeightManager.bold, color);
+
+TextStyle getSemiBoldStyle({double fontSize = FontSize.normal, required Color color}) =>
+    _getTextStyle(fontSize, FontConstants.fontFamily, FontWeightManager.semiBold, color);
--- src/lib/presentation/theme_manager.dart
+++ src/lib/presentation/theme_manager.dart
@@ -1,11 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/presentation/color_manager.dart';
+import 'package:myapp/presentation/font_manager.dart';
+import 'package:myapp/presentation/styles_manager.dart';
+import 'package:myapp/presentation/value_manager.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-      // main colors of the app
+    // main colors of the app
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.primaryOpacity70,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager.grey1,
 
-      // card view theme
+    // ripple color
+    splashColor: ColorManager.primaryOpacity70,
+    accentColor: ColorManager.grey,
 
-      // app bar theme
-      );
+    // card view theme
+    cardTheme: CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.grey,
+      elevation: AppSize.small,
+    ),
+
+    // app bar theme
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: ColorManager.primary,
+      elevation: AppSize.small,
+      shadowColor: ColorManager.primaryOpacity70,
+      titleTextStyle: getRegularStyle(color: ColorManager.white, fontSize: FontSize.heading1),
+    ),
+
+    // button theme
+    buttonTheme: const ButtonThemeData(
+      shape: StadiumBorder(),
+      // disabledColor: ColorManager.grey1,
+      // buttonColor: ColorManager.primary,
+      // splashColor: ColorManager.primaryOpacity70,
+    ),
+
+    // elevated button theme
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        textStyle: getRegularStyle(color: ColorManager.white),
+        primary: ColorManager.primary,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(AppSize.medium)),
+      ),
+    ),
+
+    // text theme
+    textTheme: TextTheme(
+      headline1: getSemiBoldStyle(fontSize: FontSize.heading1, color: ColorManager.darkGrey),
+      subtitle1: getMediumStyle(color: ColorManager.lightGrey, fontSize: FontSize.small),
+      caption: getRegularStyle(color: ColorManager.grey1),
+      bodyText1: getRegularStyle(color: ColorManager.grey),
+    ),
+
+    // input theme
+  );
 }
