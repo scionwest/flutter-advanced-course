4716b748897d9f8336a785762aba0f3ff5f5c9e2
Johnathon Sullinger <johnathon@sullinger.us>
2022-07-06 12:21:34 -0700

    Added navigation icons to asset manager

--- src/lib/presentation/resources/assets_manager.dart
+++ src/lib/presentation/resources/assets_manager.dart
@@ -1,10 +1,15 @@
 const String imagePath = 'assets/images';
 
 class ImageAssets {
-  static const String splashLogo = 'assets/images/splash_logo.png';
+  static const String splashLogo = '$imagePath/splash_logo.png';
 
-  static const String onboardingLogo1 = 'assets/images/onboarding_logo1.svg';
-  static const String onboardingLogo2 = 'assets/images/onboarding_logo2.svg';
-  static const String onboardingLogo3 = 'assets/images/onboarding_logo3.svg';
-  static const String onboardingLogo4 = 'assets/images/onboarding_logo4.svg';
+  static const String onboardingLogo1 = '$imagePath/onboarding_logo1.svg';
+  static const String onboardingLogo2 = '$imagePath/onboarding_logo2.svg';
+  static const String onboardingLogo3 = '$imagePath/onboarding_logo3.svg';
+  static const String onboardingLogo4 = '$imagePath/onboarding_logo4.svg';
+
+  static const String hollowCircle = '$imagePath/hollow_circle_ic.svg';
+  static const String solidCircle = '$imagePath/solid_circle_ic.svg';
+  static const String leftArrow = '$imagePath/left_arrow_ic.svg';
+  static const String rightArrow = '$imagePath/right_arrow_ic.svg';
 }
