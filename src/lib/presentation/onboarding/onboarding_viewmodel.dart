56f7cf991d4c296ae2338b46523b513d07b7669c
Johnathon Sullinger <johnathon@sullinger.us>
2022-09-25 17:41:33 -0700

    Stubs for onboarding viewmodels

---
+++ src/lib/presentation/onboarding/onboarding_viewmodel.dart
@@ -0,0 +1,20 @@
+import 'package:myapp/presentation/base/baseviewmodel.dart';
+
+class OnboardingViewModel extends BaseViewModel {
+  // inputs
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+
+  @override
+  void start() {
+    // TODO: implement start
+  }
+}
+
+// inputs mean the order that our view model will receive from our view
+abstract class OnBoardingViewModelInputs {}
+
+// outputs mean data or results that will be sent from our view modfel
+abstract class OnBoardingViewModelOutputs {}
